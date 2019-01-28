b7af169395c99133c5938d0a54316ce58f1c7a3f
amboraes <amboraes@hotmail.com>
2019-01-28 14:33:15 -0500

    tercer commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.set('port',process.env.port || 3000);
+app.set('port', process.env.port || 3000);
 
-app.listen(app.get('port'));
+app.get('/', (req, res) => {
+  res.send("Hi");
+});
+
+app.listen(app.get('port'),function(err, response){
+  console.log("Server running on port ", app.get('port'));
+});
